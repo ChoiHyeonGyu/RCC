ae75e5f0d597238a66f8ebb8874426c235847d06
cty815 <xodyd815@naver.com>
2019-05-17 13:46:22 +0900

    .

--- app.js
+++ app.js
@@ -14,37 +14,18 @@ app.use("/images", express.static(__dirname + '/index/images'));
 app.use("/css", express.static(__dirname + '/index/css'));
 app.use("/vendor", express.static(__dirname + '/index/vendor'));
 
-
-
 app.all("*", function (req, res, next) {
     next();
 });
-
-app.get("/", function (req, res) {//get방식으로 '/' 경로로 요청이 왔을 때    
-    var str = "welcome 인덱스 page<hr>";
-    str += "<form method='post' action=''>";
-    str += "<button>post로 인덱스 페이지에 가면??</button>";
-    str += "</form>";
-    fs.readFile("index/index.html", (err, data) => {
-        console.log("<script>console.log(123);</script>");
-        res.end(data);
-    });
-    //res.end(str)
-});
-
+app.get("/", function (req, res) {});
 app.get("/join", function (req, res) {
     fs.readFile("index/join.html", (err, data) => {
         res.end(data);
     });
 });
+app.post("/join", function (req, res) {});
 
-app.post("/join", function (req, res) {
-    console.log("Join Page")
-});
-
-app.get("/login", function (req, res) {
-    res.end("welcome Login page")
-});
+app.get("/login", function (req, res) {});
 
 http.createServer(app).listen(9999, 'localhost');
 console.log('Server On');
