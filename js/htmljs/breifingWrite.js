85513e270633e670e5f80eaab724652aedd686a3
cty815 <xodyd815@naver.com>
2019-05-20 17:34:01 +0900

    js 01

---
+++ js/htmljs/breifingWrite.js
@@ -0,0 +1,19 @@
+var count=1;
+function addPage(){
+    console.log('add');
+    if(count==10)return;
+    count++;
+    var target = document.getElementById("writeFormBox");
+    var newForm = "<div class='formDiv' id='formDiv"+count+"'>"+
+    "<div class='divBox cw65'><span class='titleWord'>HeadLine</span> <input type='text' placeholder='title'></div>"+
+    "<div class='divBox cw65'><span class='titleWord'>URL</span> <input type='text' placeholder='url'></div>"+
+    "<div class='divBox cw65'><span class='titleWord'>Summary</span> <textarea placeholder='summary'></textarea></div><div>";
+    target.innerHTML+=newForm;
+}
+function removePage(){
+    if(count==1)return;
+    var id = "formDiv"+count;
+    var target = document.getElementById(id);
+    target.parentNode.removeChild(target);
+    count--;
+}
