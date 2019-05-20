38d0e0e75a72368e53d33d6daffbe3fdee687771
Choi Hyeon Gyu <gaphasus@gmail.com>
2019-05-20 23:52:36 +0900

    nodejs 경로 설정 & 헤더 정리

--- js/nodejs/main_router.js
+++ js/nodejs/main_router.js
@@ -7,49 +7,33 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
+var import_default = '';
+
+fs.readFile("header/import_default.html", "utf-8", function(error, data) {
+    import_default = data;
+});
+
 router.get("/", function(req, res) {
     fs.readFile("main.html", "utf-8", function(error, data) {
-        res.end(data);
+        res.send(ejs.render(import_default + data));
     });
 });
 
-router.get("/imsi", function(req, res) {
-    var logo = '';
-    var main_header = '';
-    var navigator = '';
-    var navigator_side = '';
-    var footer = '';
-    var contentsSideNav = '';
+router.get("/login", function(req, res) {
+    fs.readFile("login.html", "utf-8", function(error, data) {
+        res.send(ejs.render(import_default + data));
+    });
+});
 
-    var import_default = '';
+router.get("/signup", function(req, res) {
+    fs.readFile("signup.html", "utf-8", function(error, data) {
+        res.send(ejs.render(import_default + data));
+    });
+});
 
-    fs.readFile("header/logo.html", "utf-8", function(error, data) {
-        logo = data;
-    });
-    fs.readFile("header/main_header.html", "utf-8", function(error, data) {
-        main_header = data;
-    });
-    fs.readFile("header/navigator.html", "utf-8", function(error, data) {
-        navigator = data;
-    });
-    fs.readFile("header/navigator_side.html", "utf-8", function(error, data) {
-        navigator_side = data;
-    });
-    fs.readFile("footer.html", "utf-8", function(error, data) {
-        footer = data;
-    });
-    fs.readFile("header/contentsSideNav.html", "utf-8", function(error, data) {
-        contentsSideNav = data;
-    });
-
-    fs.readFile("header/import_default.html", "utf-8", function(error, data) {
-        import_default = data;
-    });
-
+router.get("/my", function(req, res) {
     fs.readFile("mypage.html", "utf-8", function(error, data) {
-        res.send(ejs.render(import_default + data, {
-            data: "되냐?"
-        }));
+        res.send(ejs.render(import_default + data));
     });
 });
 
