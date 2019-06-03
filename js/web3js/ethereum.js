71ca19e2d334eb29f28021561ee9c20873dd1a22
A <gaphasus@gmail.com>
2019-06-03 15:54:54 +0900

    donate

--- js/web3js/ethereum.js
+++ js/web3js/ethereum.js
@@ -1,18 +1,29 @@
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-function getBalance(addr){
-    /*web3.eth.getBalance(addr, function(error, balance){
-        if(error) console.log(error);
-        web3.utils.fromWei(balance, "ether");
-    });*/
-    /*web3.eth.getBalance(addr).then(function(balance){
-        return web3.utils.fromWei(balance, "ether");
-    }, function(balance){
-        console.log(web3.utils.fromWei(balance, "ether"));
-    });*/
+function getBalance(addr, callback){
+    web3.eth.getBalance(addr, function(err, coin){
+        if(err) console.log(err);
+        callback(web3.utils.fromWei(coin, "ether"));
+    });
+}
+
+function sendCoin(sender, receiver, coin, callback){
+    web3.eth.personal.unlockAccount(sender, "1234", 300, function(err){
+        if(err) console.log(err);
+    });
+
+    web3.eth.sendTransaction({
+        from: sender,
+        to: receiver,
+        value: web3.utils.toWei(coin, "ether")
+    }, "1234", function(err, txHash){
+        if(err) console.log(err);
+        if(txHash) callback();
+    });
 }
 
 module.exports = {
-    getBalance: getBalance
+    getBalance: getBalance,
+    sendCoin: sendCoin
 }
